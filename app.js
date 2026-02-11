c196ddf884a0451b2c8be31aa6d24e8b839b6cc3
naims <naimdrive6@gmail.com>
2026-02-11 09:15:36 +0600

    setup upstash workflow

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 // import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 import cookieParser from "cookie-parser";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -19,6 +20,7 @@ app.use(cookieParser())
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 
 
 app.get("/", (req, res) => {
